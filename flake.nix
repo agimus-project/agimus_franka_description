5f6f0846be76ff4251ab242c3aa48d710365208b
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-04-10 14:47:04 +0200

    sync with gepetto/nix template

--- flake.nix
+++ flake.nix
@@ -1,40 +1,45 @@
 {
   description = "ROS integration for Franka research robots";
 
-  inputs.nix-ros-overlay.url = "github:lopsided98/nix-ros-overlay/master";
+  inputs = {
+    gepetto.url = "github:gepetto/nix";
+    flake-parts.follows = "gepetto/flake-parts";
+    systems.follows = "gepetto/systems";
+  };
 
   outputs =
-    { nix-ros-overlay, self, ... }:
-    nix-ros-overlay.inputs.flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nix-ros-overlay.inputs.nixpkgs {
-          inherit system;
-          overlays = [ nix-ros-overlay.overlays.default ];
-        };
-      in
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+      { lib, ... }:
       {
-        packages = {
-          default = self.packages.${system}.franka-description;
-          franka-description = pkgs.rosPackages.humble.franka-description.overrideAttrs {
-            src = pkgs.lib.fileset.toSource {
-              root = ./.;
-              fileset = pkgs.lib.fileset.unions [
-                ./CMakeLists.txt
-                ./end_effectors
-                ./env-hooks
-                ./launch
-                ./meshes
-                ./package.xml
-                ./robots
-                ./rviz
-                ./scripts
-                ./test
-                ./worlds
-              ];
+        systems = import inputs.systems;
+        imports = [
+          inputs.gepetto.flakeModule
+          {
+            flakoboros = {
+              rosDistros = [ "humble" ]; # TODO
+              rosShellDistro = "humble"; # TODO
+              rosOverrideAttrs.franka-description = _: _: {
+                src = lib.fileset.toSource {
+                  root = ./.;
+                  fileset = lib.fileset.unions [
+                    ./CMakeLists.txt
+                    ./end_effectors
+                    ./env-hooks
+                    ./launch
+                    ./meshes
+                    ./package.xml
+                    ./robots
+                    ./rviz
+                    ./scripts
+                    ./test
+                    ./worlds
+                  ];
+                };
+              };
             };
-          };
-        };
+          }
+        ];
       }
     );
 }
